f71bf185aa330c9d66dd1be6093d8bc9295dec07
Krabbe <lukas.krebs@hispeed.ch>
2019-04-05 18:51:24 +0200

    Day2 Task1 finished

--- Day2/Task1/Task1.php
+++ Day2/Task1/Task1.php
@@ -6,6 +6,33 @@
  * Time: 5:18 PM
  */
 $input = file('input.txt', FILE_IGNORE_NEW_LINES);
+$doubles = 0;
+$triples = 0;
 foreach ($input as $letter){
-    echo $letter. '<br/>';
+    $letterArr = str_split($letter);
+    $double = false;
+    $triple = false;
+    for($i = 0, $iMax = count($letterArr); $i < $iMax; $i++){
+        $usedChars = [];
+        if(in_array($letterArr[$i], $usedChars, true)){
+            continue;
+        }
+        $search = array_keys($letterArr, $letterArr[$i]);
+        if(count($search) === 2 && !$double){
+            $usedChars[] = $letterArr[$i];
+            $double = true;
+            ++$doubles;
+            continue;
+        }
+        if(count($search) === 3 && !$triple){
+            $usedChars[] = $letterArr[$i];
+            $triple = true;
+            ++$triples;
+            continue;
+        }
+        if($double && $triple){
+            break;
+        }
+    }
 }
+echo $doubles * $triples;
