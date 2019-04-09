ae4477165fd64c0520870e50e52d1eab6c72d513
Krabbe <lukas.krebs@hispeed.ch>
2019-04-09 22:43:53 +0200

    Day 5 Task 1 finished

--- Day5/Task1/Task1.php
+++ Day5/Task1/Task1.php
@@ -6,45 +6,87 @@
  * Time: 9:00 PM
  */
 
-ini_set('max_execution_time', 1200);
-$input = file('input.txt', FILE_IGNORE_NEW_LINES);
-$letters = str_split($input[0]);
+//THIS WORKS BUT IS WAY TO SLOW
+
+//ini_set('max_execution_time', 2000);
+//$input = file('input.txt', FILE_IGNORE_NEW_LINES);
+//$letters = str_split($input[0]);
+//$finished = false;
+//
+//$i = 0;
+//while(!$finished){
+//    $lastLetter = $letters[0];
+//
+//    $first = true;
+//    foreach($letters as $l){
+//        if($first){
+//            $first = false;
+//            continue;
+//        }
+//        $finished = false;
+//        if(ctype_upper($l)){
+//            //UC
+//            if(lcfirst($l) === $lastLetter){
+//                //Same & LC
+//                $pos = strpos($input[0], $lastLetter.$l);
+//                $input[0] = substr_replace($input[0], '', $pos, 2);
+//                $letters = str_split($input[0]);
+//                break;
+//            }
+//        } else {
+//            //LC
+//            if(ucfirst($l) === $lastLetter){
+//                //Same & UC
+//                $pos = strpos($input[0], $lastLetter.$l);
+//                $input[0] = substr_replace($input[0], '', $pos, 2);
+//                $letters = str_split($input[0]);
+//                break;
+//            }
+//        }
+//        $finished = true;
+//        $output = implode('',$letters);
+//        $lastLetter = $l;
+//    }
+//}
+//echo strlen($output);
+
+
+//Faster Version I think
+ini_set('max_execution_time', 2000);
+$input = file_get_contents('input.txt');
 $finished = false;
 
-$i = 0;
 while(!$finished){
-    $lastLetter = $letters[0];
+    $lastLetter = $input[0];
 
-    foreach($letters as $l){
-        $found = false;
-        $finished2 = false;
-        if(ctype_upper($l)){
+    $first = true;
+    for ($i = 0, $iMax = strlen($input); $i < $iMax; $i++){
+        if($first){
+            $first = false;
+            continue;
+        }
+        $finished = false;
+        if(ctype_upper($input[$i])){
             //UC
-            if(lcfirst($l) === $lastLetter){
+            if(lcfirst($input[$i]) === $lastLetter){
                 //Same & LC
-                $pos = strpos($input[0], $lastLetter.$l);
-                $newInput = substr_replace($input[0], '', $pos, 2);
-                $letters = str_split($newInput);
+                $pos = strpos($input, $lastLetter.$input[$i]);
+                $input = substr_replace($input, '', $pos, 2);
                 break;
             }
         } else {
             //LC
-            if(ucfirst($l) === $lastLetter){
+            if(ucfirst($input[$i]) === $lastLetter){
                 //Same & UC
-                $pos = strpos($input[0], $lastLetter.$l);
-                $newInput = substr_replace($input[0], '', $pos, 2);
-                $letters = str_split($newInput);
+                $pos = strpos($input, $lastLetter.$input[$i]);
+                $input = substr_replace($input, '', $pos, 2);
                 break;
             }
         }
-        $finished2 = true;
-        $output = implode('',$letters);
-        $lastLetter = $l;
-    }
-    if($finished2){
         $finished = true;
+        $lastLetter = $input[$i];
     }
 }
-echo $output;
+echo strlen($input);
 ?>
 <body style="background-color: black; color: white;"></body>
---
+++ Day5/Task2/Task2.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lkrebs
+ * Date: 08.04.2019
+ * Time: 9:00 PM
+ */
+
+//THIS WORKS BUT IS WAY TO SLOW
+
+ini_set('max_execution_time', 2000);
+$input = file_get_contents('input.txt');
+$chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+$minChars = 0;
+foreach($chars as $c){
+    $round = $input;
+    $round = str_replace(array($c, ucfirst($c)), '', $round);
+    $finished = false;
+    while(!$finished){
+        $lastLetter = $round[0];
+
+        $first = true;
+        for ($i = 0, $iMax = strlen($round); $i < $iMax; $i++){
+            if($first){
+                $first = false;
+                continue;
+            }
+            $finished = false;
+            if(ctype_upper($round[$i])){
+                //UC
+                if(lcfirst($round[$i]) === $lastLetter){
+                    //Same & LC
+                    $pos = strpos($round, $lastLetter.$round[$i]);
+                    $round = substr_replace($round, '', $pos, 2);
+                    break;
+                }
+            } else {
+                //LC
+                if(ucfirst($round[$i]) === $lastLetter){
+                    //Same & UC
+                    $pos = strpos($round, $lastLetter.$round[$i]);
+                    $round = substr_replace($round, '', $pos, 2);
+                    break;
+                }
+            }
+            $finished = true;
+            $lastLetter = $round[$i];
+        }
+    }
+    if($minChars > strlen($round)){
+        $minChars = strlen($round);
+    }
+}
+
+echo $minChars;
+?>
+
+<body style="background-color: black; color: white;"></body>
