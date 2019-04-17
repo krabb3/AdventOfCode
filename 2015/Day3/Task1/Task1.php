a8907516bb3f63c686202ddbf0d35517045f4d8f
lkrebs <lukas.krebs@bertschi.com>
2019-04-17 12:10:50 +0200

    Merge branch 'master' of C:\Users\lkrebs\PhpstormProjects\AdventOfCode with conflicts.

--- 2015/Day3/Task1/Task1.php
+++ 2015/Day3/Task1/Task1.php
@@ -6,46 +6,10 @@
  * Time: 8:40 PM
  */
 $input = file('input.txt');
-$input = str_split($input[0]);
-
-$currentX = 0;
-$currentY = 0;
-
-$houses[$currentX][$currentY] = 1;
-$count = 1;
-foreach ($input as $direction){
-   switch ($direction){
-       case '^':
-           ++$currentX;
-           if(!isset($houses[$currentX][$currentY])){
-               $count++;
-           }
-           ++$houses[$currentX][$currentY];
-           break;
-       case '>':
-           ++$currentY;
-           if(!isset($houses[$currentX][$currentY])){
-               $count++;
-           }
-           ++$houses[$currentX][$currentY];
-           break;
-       case 'v':
-           --$currentX;
-           if(!isset($houses[$currentX][$currentY])){
-               $count++;
-           }
-           ++$houses[$currentX][$currentY];
-           break;
-       case '<':
-           --$currentY;
-           if(!isset($houses[$currentX][$currentY])){
-               $count++;
-           }
-           ++$houses[$currentX][$currentY];
-           break;
-       default:
-           die('ERROR');
-           break;
-   }
+$count = 0;
+foreach ($input as $multiplication){
+   $multiplication = explode('x', $multiplication);
+   $count += min(array($multiplication[0] * $multiplication[1], $multiplication[0] * $multiplication[2], $multiplication[1] * $multiplication[2]));
+   $count += (2 * $multiplication[0] * $multiplication[1]) + (2 * $multiplication[0] * $multiplication[2]) + (2 * $multiplication[1] * $multiplication[2]);
 }
 echo $count;
