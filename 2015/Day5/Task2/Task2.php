8b71154d95ec9eed4039573bbec3af4616225fd9
lkrebs <lukas.krebs@bertschi.com>
2019-04-17 14:55:14 +0200

    AdventOfCode day 5 completed

--- 2015/Day5/Task2/Task2.php
+++ 2015/Day5/Task2/Task2.php
@@ -8,26 +8,29 @@
 $input = file('input.txt');
 
 $count = 0;
-$chars = [];
+//$chars = [];
 foreach ($input as $word){
 
     $word = str_replace("\r\n", '', $word);
-    $word = str_split($word);
+    $wordArr = str_split($word);
 
     $test1 = false;
     $test2 = false;
-    for ($i = 0, $iMax = count($word); $i < $iMax; $i++){
-        $chars[$word[$i]][$word[$i + 1]]++;
-        if($chars[$word[$i]][$word[$i + 1]] > 1){
+    for ($i = 0, $iMax = count($wordArr); $i < $iMax; $i++){
+        if(substr_count($word, $wordArr[$i] . $wordArr[$i + 1], $i) > 1){
             $test1 = true;
         }
-        if($word[$i] === $word[$i + 2]){
+//        $chars[$word[$i]][$word[$i + 1]]++;
+//        if($chars[$word[$i]][$word[$i + 1]] > 1){
+//            $test1 = true;
+//        }
+        if($wordArr[$i] === $wordArr[$i + 2]){
             $test2 = true;
         }
     }
     if($test1 && $test2){
         $count++;
     }
-    unset($chars);
+//    unset($chars);
 }
 echo $count;
